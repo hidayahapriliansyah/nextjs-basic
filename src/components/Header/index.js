18a6f21e4b5e4f8db0e0105bc3a0151ef2a1b7de
hidayahapriliansyah <adimuhamadfirmansyah@gmail.com>
2023-04-02 09:00:13 +0700

    08-global-css-and-css-module

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,16 +1,17 @@
 import Link from 'next/link';
+import style from './Header.module.css';
 
 function Header() {
   return (
-    <header>
-      <ul>
-        <li>
+    <header className={style.container}>
+      <ul className={style.list}>
+        <li className={style.item}>
           <Link href="/users/detail">Users Detail</Link>
         </li>
-        <li>
+        <li className={style.item}>
           <Link href="/users">Users</Link>
         </li>
-        <li>
+        <li className={style.item}>
           <Link href="/about">About</Link>
         </li>
       </ul>
