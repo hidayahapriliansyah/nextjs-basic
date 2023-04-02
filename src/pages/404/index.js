9435b56af4d1b83f36baa79a31eedbfe9b47cc88
hidayahapriliansyah <adimuhamadfirmansyah@gmail.com>
2023-04-02 09:54:36 +0700

    11-auto-redirection

--- src/pages/404/index.js
+++ src/pages/404/index.js
@@ -1,4 +1,14 @@
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+
 export default function NotFound() {
+  const router = useRouter();
+  useEffect(() => {
+    setTimeout(() => {
+      router.push('/');
+    }, 3000);
+  }, []);
+
   return (
     <div>404</div>
   );
