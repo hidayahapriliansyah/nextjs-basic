cb2b44d69ad5adbf29b87e59aa0cc608c7605281
hidayahapriliansyah <adimuhamadfirmansyah@gmail.com>
2023-04-02 10:27:40 +0700

    13-optimalisasi-image

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,9 +1,108 @@
+import Image from 'next/image';
 import Layout from '../components/Layout';
 
 function about() {
   return (
     <Layout pageTitle="About">
-      <div>about</div>
+      <>
+        <div>about</div>
+        <Image src="/image1.png" width={200} height={200} alt="image" />
+        <img src="/image1.png" width={200} height={200} alt="imageagai" />
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, unde.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, unde.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, unde.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, unde.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, unde.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, unde.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, unde.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, unde.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, unde.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, unde.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, unde.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, unde.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, unde.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, unde.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, unde.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, unde.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, unde.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, unde.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, unde.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, unde.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, unde.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, unde.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, unde.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, unde.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, unde.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, unde.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, unde.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, unde.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, unde.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, unde.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, unde.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, unde.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, unde.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, unde.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, unde.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, unde.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, unde.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, unde.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, unde.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, unde.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, unde.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, unde.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, unde.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, unde.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, unde.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, unde.</p>
+        <Image src="/image2.png" width={200} height={200} alt="image" />
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, unde.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, unde.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, unde.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, unde.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, unde.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, unde.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, unde.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, unde.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, unde.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, unde.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, unde.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, unde.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, unde.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, unde.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, unde.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, unde.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, unde.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, unde.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, unde.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, unde.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, unde.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, unde.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, unde.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, unde.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, unde.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, unde.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, unde.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, unde.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, unde.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, unde.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, unde.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, unde.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, unde.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, unde.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, unde.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, unde.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, unde.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, unde.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, unde.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, unde.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, unde.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, unde.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, unde.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, unde.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, unde.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, unde.</p>
+        <Image src="/image3.jpg" width={200} height={200} alt="imagesdfsdf" />
+      </>
     </Layout>
   );
 }
