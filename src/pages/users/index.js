b034c9e26a9a600a5ce7568eb6870fd93786942b
hidayahapriliansyah <adimuhamadfirmansyah@gmail.com>
2023-04-02 11:49:37 +0700

    15-data-fetching-getStaticProps

--- src/pages/users/index.js
+++ src/pages/users/index.js
@@ -1,11 +1,31 @@
 import Layout from '../../components/Layout';
 
-function about() {
+function user({ dataUsers }) {
   return (
     <Layout pageTitle="User Index">
       <div>User Index</div>
+      <ul>
+        {dataUsers.map((userData) => (
+          <li>
+            {userData.name}
+            ||
+            {userData.email}
+          </li>
+        ))}
+      </ul>
     </Layout>
   );
 }
 
-export default about;
+export default user;
+
+export async function getStaticProps() {
+  const res = await fetch('https://jsonplaceholder.typicode.com/users');
+  const dataUsers = await res.json();
+
+  return {
+    props: {
+      dataUsers,
+    },
+  };
+}
