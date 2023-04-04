dc4db0ddca197bf869c4445cd2be9c966dc2114d
hidayahapriliansyah <adimuhamadfirmansyah@gmail.com>
2023-04-04 13:08:30 +0700

    geuning error ah

--- src/pages/users/[id].js
+++ src/pages/users/[id].js
@@ -1,18 +1,47 @@
-import { useRouter } from 'next/router';
 import Layout from '../../components/Layout';
 
-function about() {
-  const router = useRouter();
-  const { id } = router.query;
+function UserDetail(props) {
+  // eslint-disable-next-line react/prop-types
+  const { user } = props;
   return (
     <Layout pageTitle="User Detail">
       <div>
-        User Detail
-        {' '}
-        {id}
+        <p>{user.name}</p>
+        <p>{user.email}</p>
+        <p>{user.phone}</p>
+        <p>{user.website}</p>
       </div>
     </Layout>
   );
 }
 
-export default about;
+export default UserDetail;
+
+export async function getStaticPaths() {
+  const res = await fetch('https://jsonplaceholder.typicode.com/users');
+  const dataUsers = await res.json();
+
+  const paths = dataUsers.map((user) => ({
+    params: {
+      id: `${user.id}`,
+    },
+  }));
+
+  return {
+    paths,
+    fallbacK: true,
+  };
+}
+
+export async function getStaticProps(context) {
+  const { id } = context.params;
+
+  const res = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`);
+  const user = await res.json();
+
+  return {
+    props: {
+      user,
+    },
+  };
+}
--- src/pages/users/index.js
+++ src/pages/users/index.js
@@ -1,12 +1,16 @@
+import { useRouter } from 'next/router';
 import Layout from '../../components/Layout';
+import styles from '../../styles/Users.module.css';
 
 function user({ dataUsers }) {
+  const router = useRouter();
+
   return (
     <Layout pageTitle="User Index">
       <div>User Index</div>
       <ul>
         {dataUsers.map((userData) => (
-          <li>
+          <li key={userData.id} onClick={() => router.push(`users/${userData.id}`)} className={styles.card}>
             {userData.name}
             ||
             {userData.email}
